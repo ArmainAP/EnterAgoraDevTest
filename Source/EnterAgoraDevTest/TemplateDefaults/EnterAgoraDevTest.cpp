c9af02e27c7d3be0b3b78f5000341d13440cb5d4
ArmainAP <adrianmarianpopoviciu@gmail.com>
2020-09-17 18:22:48 +0300

    *Moved template generate code to its own folder.

---
+++ Source/EnterAgoraDevTest/TemplateDefaults/EnterAgoraDevTest.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EnterAgoraDevTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EnterAgoraDevTest, "EnterAgoraDevTest" );
+
+DEFINE_LOG_CATEGORY(LogEnterAgoraDevTest)
+ 
---
+++ Source/EnterAgoraDevTest/TemplateDefaults/EnterAgoraDevTestGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EnterAgoraDevTestGameMode.generated.h"
+
+UCLASS(MinimalAPI)
+class AEnterAgoraDevTestGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AEnterAgoraDevTestGameMode();
+};
+
+
+
